b0e7cc93be8934f93845fb7cce9708be7348012c
ajoyous <ajoy.gonsalves@hotmail.com>
2024-06-10 23:46:12 +0200

    build: add exercise 3.8

--- index.js
+++ index.js
@@ -32,7 +32,13 @@ const generateId = () => {
 
 app.use(express.json());
 
-app.use(morgan("tiny"));
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.use((req, res, next) => {
   const options = { timeZoneName: "long" };
